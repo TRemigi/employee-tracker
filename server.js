6875ed9f284b37ddc79eb64c3ad2750956bdec85
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-07-18 20:48:20 -0600

    added simple route to server.js for testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connection = require('./db/database');
-const PORT = process.env.PORT || parseInt(process.env.port);
+const PORT = process.env.PORT || 3000;
 const app = express();
 const apiRoutes = require('./apiRoutes');
 
@@ -18,10 +18,12 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-
+app.get('/', (req, res) => {
+    res.json({ message: 'success' });
+});
 
 // start server after DB connection
-connection.on('ready', () => {
+connection.on('start', () => {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
