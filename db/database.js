00c2e0af4bc450a944aabf68dc59ed5213f5ad07
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-07-20 16:34:02 -0600

    Update database.js

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Tj345734571124842!',
+    password: '',
     database: 'employees_DB'
 });
 
